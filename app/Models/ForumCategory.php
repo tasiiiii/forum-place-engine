973407659c32c15cc412ebe0ed3827a621cbf100
Default Name <tasiiiii.net@gmail.com>
2023-01-15 17:56:18 +0300

    Added ForumCategory model

---
+++ app/Models/ForumCategory.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @property int    $id
+ * @property string $title
+ * @property string $description
+ * @property string $icon
+ * @property int    $creator_id
+ */
+class ForumCategory extends Model
+{}
