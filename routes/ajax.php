4499ad49ae8b2f7a839b18c458b9ce77e8212391
Default Name <tasiiiii.net@gmail.com>
2023-02-14 23:40:44 +0300

    #

--- routes/ajax.php
+++ routes/ajax.php
@@ -2,11 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::group(['prefix' => 'registration'], function () {
+Route::group(['prefix' => 'registration', 'middleware' => 'guest'], function () {
     Route::get('name-checker', [\App\Http\Controllers\Ajax\Common\Registration\NameChecker\Controller::class, 'run'])->name('name_checker_ajax');
 });
 
 Route::group(['prefix' => 'forum'], function () {
     Route::get('topic-list/{alias}', [\App\Http\Controllers\Ajax\Forum\Topic\ShowList\Controller::class, 'run'])->name('topic_list_ajax');
-    Route::post('topic/reaction/create', [\App\Http\Controllers\Ajax\Forum\Topic\Reaction\Create\Controller::class, 'run'])->name('create_topic_reaction');
+    Route::post('topic/reaction/create', [\App\Http\Controllers\Ajax\Forum\Topic\Reaction\Create\Controller::class, 'run'])
+        ->middleware('auth')
+        ->name('create_topic_reaction');
 });
